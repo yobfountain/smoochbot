0a31cd69e61a15a0c0e4f8bb9e78c3b9c390c10c
Brian Fountain <brian@brianfountain.com>
2010-10-03 11:47:07 -0400

    commented out gmail envionment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -4,14 +4,14 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Smoochbot::Application.initialize!
 
-Smoochbot::Application.configure do
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    :address              => "smtp.gmail.com",
-    :port                 => 587,
-    :domain               => 'brianfountain.com',
-    :user_name            => 'vpotus@gmail.com',
-    :password             => 'fourteen',
-    :authentication       => 'plain',
-    :enable_starttls_auto => true  }
-end
+# Smoochbot::Application.configure do
+#   config.action_mailer.delivery_method = :smtp
+#   config.action_mailer.smtp_settings = {
+#     :address              => "smtp.gmail.com",
+#     :port                 => 587,
+#     :domain               => 'brianfountain.com',
+#     :user_name            => 'vpotus@gmail.com',
+#     :password             => 'fourteen',
+#     :authentication       => 'plain',
+#     :enable_starttls_auto => true  }
+# end
